9837099684ae4a502906ed7681b8b2e0bd477273
arkutty <akutty@umich.edu>
2025-04-10 23:04:50 -0400

    Add files via upload
    Signed-off-by: arkutty <akutty@umich.edu>

---
+++ person.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 University of Michigan EECS183
+ *
+ * Person.cpp
+ * Project UID 848fee0125dbb5eb53ed294f20dbef81
+ *
+ * Thomas Cosgrove
+ * tomcos
+ *
+ * Final Project - Elevators
+ */
+
+#include "Person.h"
+#include <iostream>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+Person::Person(string inputString) : Person() {
+    stringstream ss(inputString);
+    char temp;
+    
+    ss >> turn >> temp >> currentFloor >> temp >> targetFloor >> temp >> angerLevel;
+}
+
+bool Person::tick(int currentTime) {
+    if (currentTime % TICKS_PER_ANGER_INCREASE == 0) {
+        angerLevel++;
+    }
+    return angerLevel >= MAX_ANGER;
+}
+
+void Person::print(ostream &outs) {
+    outs << "f" << currentFloor << "t" << targetFloor << "a" << angerLevel;
+}
+
+//////////////////////////////////////////////////////
+////// DO NOT MODIFY ANY CODE BENEATH THIS LINE //////
+//////////////////////////////////////////////////////
+
+Person::Person() {
+    turn = 0;
+    currentFloor = 0;
+    targetFloor = 0;
+    angerLevel = 0;
+}
+
+int Person::getTurn() const {
+    return turn;
+}
+
+int Person::getCurrentFloor() const {    
+    return currentFloor;
+}
+
+int Person::getTargetFloor() const {    
+    return targetFloor;
+}
+
+int Person::getAngerLevel() const {    
+    return angerLevel;
+}
+
+ostream& operator<< (ostream& outs, Person p)
+{
+    p.print(outs);
+    return outs;
+}
